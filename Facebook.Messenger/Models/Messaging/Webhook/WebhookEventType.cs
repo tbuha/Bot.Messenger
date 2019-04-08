95844e2c7a6c54b458b55568ac7fe58af09a7aff
Taras B <Taras.Buha@gmail.com>
2019-04-08 21:14:48 +0300

    Added .net standart poject

---
+++ Facebook.Messenger/Models/Messaging/Webhook/WebhookEventType.cs
@@ -0,0 +1,18 @@
+﻿namespace Facebook.Messenger.Models
+{
+    public enum WebhookEventType
+    {
+        UnIdentifiedCallback,
+        MessageReceivedCallback,
+        MessageDeliveredCallback,
+        MessageReadCallback,
+        MessageEchoCallback,
+        PostbackRecievedCallback,
+        PluginOptinCallback,
+        ReferralCallback,
+        PaymentCallback,
+        CheckoutUpdateCallback,
+        PreCheckoutCallback,
+        AccountLinkingCallback
+    }
+}
